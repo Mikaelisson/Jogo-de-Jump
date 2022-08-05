fe8d18adb860b2b1435ec95fa2da763895e52766
Mikaelisson <mikaelissongesuino@gmail.com>
2022-08-05 20:46:08 -0300

    Adicionado tela de Inicio e correção de bugs

---
+++ scripts/config.js
@@ -0,0 +1,70 @@
+const config = document.getElementById("config");
+let LOAD_RECORD =
+  localStorage.getItem(SAVE_RECORD) === null
+    ? 0
+    : localStorage.getItem(SAVE_RECORD);
+
+const createConfig = () => {
+  const configElement = document.createElement("div");
+  configElement.classList.add("config-element");
+  configElement.innerHTML = `
+    <h1>
+        <span style="color: green">S</span><span style="color: aqua">u</span
+        ><span style="color: rgb(255, 217, 0)">p</span
+        ><span style="color: red">e</span><span style="color: rgb(255, 217, 0)">r</span
+        >
+    </h1>
+    <h1>
+      <span style="color: green">J</span><span style="color: aqua">u</span
+      ><span style="color: rgb(255, 217, 0)">m</span
+      ><span style="color: red">p</span>
+      <span style="color: rgb(255, 217, 0)">M</span
+      ><span style="color: aqua">a</span><span style="color: green">r</span
+      ><span style="color: red">i</span><span style="color: green">o</span>
+    </h1>
+     <button id="start" onclick="start()">Iniciar</button>
+    `;
+
+  const pontuacao = document.createElement("div");
+  pontuacao.classList.add("pontuacao");
+
+  const h2Title = document.createElement("h2");
+  h2Title.innerHTML = "Última Pontuação";
+
+  const h2Value = document.createElement("h2");
+  setInterval(() => {
+    h2Value.innerHTML = LOAD_RECORD;
+  }, 500);
+
+  // const btnStart = document.createElement('button');
+  // btnStart.id = 'start';
+  // btnStart.onclick =  start();
+  // btnStart.innerHTML = 'Iniciar';
+
+  pontuacao.appendChild(h2Title);
+  pontuacao.appendChild(h2Value);
+  // pontuacao.appendChild(btnStart);
+
+  // <div class="pontuacao">
+  //     <h2>Última Pontuação</h2>
+  //     <h2>${LOAD_RECORD}</h2>
+  // </div>
+
+  configElement.appendChild(pontuacao);
+  config.appendChild(configElement);
+};
+
+setInterval(() => {
+  LOAD_RECORD =
+    localStorage.getItem(SAVE_RECORD) === null
+      ? 0
+      : localStorage.getItem(SAVE_RECORD);
+}, 100);
+
+function start() {
+  config.style.display = "none";
+  score = 0;
+  teste();
+}
+
+document.addEventListener("DOMContentLoaded", createConfig);
--- scripts/script.js
+++ scripts/script.js
@@ -3,60 +3,84 @@ const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const clouds = document.querySelector(".clouds");
 const JUMP = "jump";
-const SAVE_RECORD = 'save_record';
+const SAVE_RECORD = "save_record";
 let score = null;
 
-const jump = (event) => {
-  if (event.keyCode === 32) {
-    mario.classList.add(JUMP);
+function teste() {
+    
 
-    setTimeout(() => {
-      mario.classList.remove(JUMP);
-    }, 500);
-  }
-};
+    pipe.style.animation = 'pipe-animation 2s infinite linear';
+    pipe.style.left = '';
 
-const loop = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
-  const cloudsPosition = clouds.offsetLeft;
+    mario.style.animation = '';
+    mario.style.bottom = '0';
+    mario.src = "./assets/img/mario.gif";
+    mario.style.width = "100px";
+    mario.style.marginLeft = '';
 
-  if (pipePosition <= 80 && pipePosition > 0 && marioPosition <= 65) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+    clouds.style.animation = 'clouds-animation 10s linear infinite';
+    clouds.style.left = '';
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
-    mario.src = "./assets/img/game-over.png";
-    mario.style.width = "55px";
-    mario.style.marginLeft = "40px";
 
-    clouds.style.animation = "none";
-    clouds.style.left = `${cloudsPosition}px`;
 
-    localStorage.setItem(SAVE_RECORD, score)
 
-    clearInterval(loop);
-  } else {
-    if (pipePosition < 0) return score++;
-  }
-}, 10);
+
+    
+  const jump = (event) => {
+    if (event.keyCode === 32) {
+      mario.classList.add(JUMP);
+
+      setTimeout(() => {
+        mario.classList.remove(JUMP);
+      }, 500);
+    }
+  };
+
+  const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+      .getComputedStyle(mario)
+      .bottom.replace("px", "");
+    const cloudsPosition = clouds.offsetLeft;
+
+    if (pipePosition <= 80 && pipePosition > 0 && marioPosition <= 65) {
+      pipe.style.animation = "none";
+      pipe.style.left = `${pipePosition}px`;
+
+      mario.style.animation = "none";
+      mario.style.bottom = `${marioPosition}px`;
+      mario.src = "./assets/img/game-over.png";
+      mario.style.width = "55px";
+      mario.style.marginLeft = "40px";
+
+      clouds.style.animation = "none";
+      clouds.style.left = `${cloudsPosition}px`;
+      if (score === null) {
+        score = 0;
+        localStorage.setItem(SAVE_RECORD, score);
+      } else {
+        localStorage.setItem(SAVE_RECORD, score);
+      }
+      config.style.display = "flex";
+      clearInterval(loop);
+    } else {
+      if (pipePosition < 0) return score++;
+    }
+  }, 10);
+
+
+  document.addEventListener("keydown", jump);
+}
+
+
 
 const scoreboard = () => {
-  const classificationElement = document.createElement("div");
-  classificationElement.classList.add("classification");
-  setInterval(() => {
-    classificationElement.innerHTML = score;
-  }, 200);
-  gameBoard.appendChild(classificationElement);
-};
+    const classificationElement = document.createElement("div");
+    classificationElement.classList.add("classification");
+    setInterval(() => {
+      classificationElement.innerHTML = score;
+    }, 200);
+    gameBoard.appendChild(classificationElement);
+  };
 
-const inicializar = () => {
-  scoreboard();
-};
-
-document.addEventListener("keydown", jump);
-
-document.addEventListener("DOMContentLoaded", inicializar);
+  document.addEventListener('DOMContentLoaded', scoreboard)
